5df6c953ef93951d9c7db232bd524aad94399483
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-01-15 18:40:33 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,55 @@
 #![feature(slice_from_ptr_range)] // shader
 #![allow(incomplete_features)]#![feature(inherent_associated_types)] // shader uniforms
 
-use {ui::{Error, throws, Result, xy, uint2, int2, image::{self, bgr}, Image}, vector::vec2};
+use {ui::{Error, throws, Result, xy, size, int2}, vector::vec2};
 use {std::sync::Arc, ui::vulkan::{Context, Commands, ImageView, buffer, Subbuffer, BufferUsage, BufferContents, Vertex}};
 
-#[derive(Clone, Copy, BufferContents, Vertex)] #[repr(C)] pub struct Height {
-	#[format(R32_SFLOAT)] pub height: f32
-}
+#[derive(Clone, Copy, BufferContents, Vertex)] #[repr(C)] pub struct Height { #[format(R32_SFLOAT)] pub height: f32 }
 
 ui::shader!{terrain, Height, Terrain}
 
 struct App {
-	#[allow(dead_code)] height: Image<Box<[u32]>>,
+	size: size,
+	height: Subbuffer::<[Height]>,
 	view_position: vec2,
 	yaw: f32,
 }
 
 impl App {
-	#[throws] fn new(_context: &Context, height: Image<Box<[u32]>>) -> Self { Self{height, view_position: xy{x: 0., y: 0.}, yaw: 0.} }
+	#[throws] fn new(context: &Context, height: &[f32]) -> Self { 
+		//let [Some(&min), Some(&max)] = [height.iter().filter(|&&v| v>=0.).min_by(|a,b| f32::total_cmp(a,b)), height.iter().max_by(|a,b| f32::total_cmp(a,b))] else {unreachable!()};
+		let [min, max] = [341.97717f32, 863.59375f32];
+		//println!("{}ms", start.elapsed().as_millis());
+		Self{
+			size: xy{x:4480, y:4240},
+			height: buffer(context, BufferUsage::VERTEX_BUFFER, height.len() as _, height.iter().map(|h| Height{height: (h-min)/(max-min)}))?,
+			view_position: xy{x: 0., y: 0.}, yaw: 0.
+		}
+	}
 }
 
 impl ui::Widget for App {
-fn paint(&mut self, context/*@Context{device, memory_allocator, ..}*/: &Context, commands: &mut Commands, target: Arc<ImageView>, _: uint2, _: int2) -> Result<()> {
-	let Self{height: _, view_position, yaw} = self;
+fn paint(&mut self, context/*@Context{device, memory_allocator, ..}*/: &Context, commands: &mut Commands, target: Arc<ImageView>, _: size, _: int2) -> Result<()> {
+	let Self{size, height, view_position, yaw} = self;
 	
 	let terrain = Terrain::new(context)?;
-	let grid = vec![0,1,2,3];
-	let grid = buffer::<u32>(context, BufferUsage::INDEX_BUFFER, grid.len() as _, grid)?;
-	
-	let height = [Height{height: 1./4.},Height{height: 2./4.},Height{height: 3./4.},Height{height: 1.}];
-	let height : Subbuffer::<[Height]> = buffer(context, BufferUsage::VERTEX_BUFFER, height.len() as _, height)?;
-	
+	let stride = size.x;
+	let grid = buffer(context, BufferUsage::INDEX_BUFFER, (size.y-1)*(size.x-1)*6, 
+		(0..size.y-1).map(|y| (0..size.x-1).map(move |x| {
+			let i0 = y*stride+x;
+			[i0, i0+1, i0+stride+1, i0, i0+stride+1, i0+stride].into_iter()
+		})).flatten().flatten()
+	)?;
+
 	//*view_position += rotate(-*yaw, control);
-	let size = {let [x,y,_] = target.image().extent(); xy{x,y}};
+	let image_size = {let [x,y,_] = target.image().extent(); xy{x,y}};
 			
 	terrain.begin_rendering(context, commands, target.clone(), &Terrain::Uniforms{
-					aspect_ratio: size.x as f32/size.y as f32,
-					view_position: (*view_position).into(),
-					yaw_sincos: xy::from(yaw.sin_cos()).into()
-				})?;
+		grid_size: (*size).into(),
+		aspect_ratio: image_size.x as f32/image_size.y as f32,
+		view_position: (*view_position).into(),
+		yaw_sincos: xy::from(yaw.sin_cos()).into()
+	})?;
 	commands.bind_vertex_buffers(0, height.clone())?;
 	commands.bind_index_buffer(grid.clone())?;
 	unsafe{commands.draw_indexed(grid.len() as _, 1, 0, 0, 0)}?;
@@ -49,16 +60,8 @@ fn paint(&mut self, context/*@Context{device, memory_allocator, ..}*/: &Context,
 
 fn main() -> Result {
 	let name = format!("{}.f32", std::env::args().skip(1).next().unwrap_or("data/DTM_R.tif.tif.exr".to_owned()));
-	let data = std::env::current_dir()?.ancestors().find_map(|root| std::fs::read(root.join(&name)).ok() ).unwrap();
-	let image = Image::new(xy{x:4480, y:4240}, bytemuck::cast_slice::<_,f32>(&data));
-	//let [Some(&min), Some(&max)] = [image.data.iter().filter(|&&v| v>=0.).min_by(|a,b| f32::total_cmp(a,b)), image.data.iter().max_by(|a,b| f32::total_cmp(a,b))] else {unreachable!()};
-	let [min, max] = [341.97717f32, 863.59375f32];
-	let oetf = &image::sRGB8_OETF12;
-	//let start = std::time::Instant::now();
-	let image = Image::from_iter(image.size, image.data.iter().map(|&v| {
-		let v = image::oetf8_12(oetf, ((v-min)/(max-min)).clamp(0., 1.));
-		bgr{b: v, g: v, r: v}.into()
-	})); // 200ms
-	//println!("{}ms", start.elapsed().as_millis());
-	ui::run(&name, Box::new(move |context| Ok(Box::new(App::new(context, image)?))))
+	ui::run(&name.clone(), Box::new(move |context| {
+			let height = std::env::current_dir()?.ancestors().find_map(|root| std::fs::read(root.join(&name)).ok() ).unwrap();
+			Ok(Box::new(App::new(context, bytemuck::cast_slice(&height))?))
+	}))
 }
--- src/terrain.wgsl
+++ src/terrain.wgsl
@@ -1,18 +1,14 @@
-struct Uniforms { view_position: vec2<f32>, yaw_sincos: vec2<f32>, aspect_ratio: f32 }
+struct Uniforms { grid_size: vec2<u32>, view_position: vec2<f32>, yaw_sincos: vec2<f32>, aspect_ratio: f32 }
 @group(0) @binding(0) var<uniform> uniforms: Uniforms;
-
-struct Height {
- @location(0) height: f32,
-}
 
 struct VertexOutput {
 	@builtin(position) position: vec4<f32>,
 	@location(0) color: vec3<f32>,
 }
 
-@vertex fn vertex(@builtin(vertex_index) vertex_index: u32, vertex: Height) -> VertexOutput {
-	let texture_coordinates = vec2(f32(vertex_index >> 1), f32(vertex_index & 1)) * 2.;
-	return VertexOutput(vec4(texture_coordinates * vec2(2., -2.) + vec2(-1., 1.), 0., 1.), vec3(vertex.height));
+@vertex fn vertex(@builtin(vertex_index) vertex_index: u32, @location(0) height: f32) -> VertexOutput {
+	let zero_one = vec2(f32(vertex_index % uniforms.grid_size.x), f32(vertex_index / uniforms.grid_size.x)) / vec2<f32>(uniforms.grid_size);
+	return VertexOutput(vec4(zero_one * 2. - 1., 0., 1.), vec3(height));
 }
 
 @fragment fn fragment(vertex_output: VertexOutput) -> @location(0) vec4<f32> {
