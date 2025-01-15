8a7de8e0c7fdf619a2fe19f5ca0c43e0709d1246
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-01-15 13:17:25 +0100

    fixe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![feature(slice_from_ptr_range)] // shader
-use ui::{Result, uint2, int2, xy, image::{self, bgr}, Image};
+use ui::{Result, xy, uint2, int2, image::{self, bgr}, Image};
 use {std::sync::Arc, ui::vulkan::{Context, Commands, ImageView}};
 
 ui::shader!{terrain, Terrain}
@@ -7,8 +7,8 @@ ui::shader!{terrain, Terrain}
 struct App(Image<Box<[u32]>>); 
 impl ui::Widget for App {
 fn paint(&mut self, context/*@Context{device, memory_allocator, ..}*/: &Context, commands: &mut Commands, target: Arc<ImageView>, _: uint2, _: int2) -> Result<()> {
-	let triangle = Triangle::new(context)?;
-	triangle.begin_rendering(context, commands, target.clone(), &[])?;
+	let terrain = Terrain::new(context)?;
+	terrain.begin_rendering(context, commands, target.clone(), &[])?;
 	unsafe{commands.draw(4, 1, 0, 0)}?;
 	commands.end_rendering()?;
 	Ok(())
@@ -16,16 +16,9 @@ fn paint(&mut self, context/*@Context{device, memory_allocator, ..}*/: &Context,
 }
 
 fn main() -> Result {
-	let terrain = std::env::args().skip(1).next().unwrap_or("data/DTM_R.tif.tif.exr".to_owned());
-	let mmap = format!("{dtm}.f32");
-	/*#[cfg(feature="exr")] if !std::fs::exists(&mmap)? {
-		let start = std::time::Instant::now();
-		let image = image::f32(dtm)?;
-		println!("{}ms {}", start.elapsed().as_millis(), image.size);
-		std::fs::write(&mmap, bytemuck::cast_slice(&image.data))?;
-	}*/
-	let mmap = std::fs::read(mmap)?;
-	let image = Image::new(xy{x:4480, y:4240}, bytemuck::cast_slice::<_,f32>(&mmap));
+	let name = format!("{}.f32", std::env::args().skip(1).next().unwrap_or("data/DTM_R.tif.tif.exr".to_owned()));
+	let data = std::env::current_dir()?.ancestors().find_map(|root| std::fs::read(root.join(&name)).ok() ).unwrap();
+	let image = Image::new(xy{x:4480, y:4240}, bytemuck::cast_slice::<_,f32>(&data));
 	//let [Some(&min), Some(&max)] = [image.data.iter().filter(|&&v| v>=0.).min_by(|a,b| f32::total_cmp(a,b)), image.data.iter().max_by(|a,b| f32::total_cmp(a,b))] else {unreachable!()};
 	let [min, max] = [341.97717f32, 863.59375f32];
 	let oetf = &image::sRGB8_OETF12;
@@ -35,5 +28,5 @@ fn main() -> Result {
 		bgr{b: v, g: v, r: v}.into()
 	})); // 200ms
 	//println!("{}ms", start.elapsed().as_millis());
-	ui::run(&terrain, &mut App(image))
+	ui::run(&name, &mut App(image))
 }
